fa8254a83b7119ab246e9fb12477491c3f53f4c0
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2022-12-11 15:23:36 +0100

    Section 4 - Bases de l'utilisation d'une BDD PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Event } from './event.entity';
 import { EventsController } from './events.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'events-nestjs',
+      entities: [Event],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Event]),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
