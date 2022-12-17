5cd743b165369c2aef6f6dbb65284627b9a09f75
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2022-12-17 11:34:01 +0100

    Section 8 - Relation @ManyToMany

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -1,5 +1,7 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Subject } from 'src/school/subject.entity';
+import { Teacher } from 'src/school/teacher.entity';
 import { Attendee } from './../events/attendee.entity';
 import { Event } from './../events/event.entity';
 
@@ -12,7 +14,7 @@ export default registerAs(
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [Event, Attendee],
+    entities: [Event, Attendee, Subject, Teacher],
     synchronize: false,
   }),
 );
