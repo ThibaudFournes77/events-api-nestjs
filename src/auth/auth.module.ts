bab3eb44ac1aca1fdc2640b56de906b269a971a0
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2022-12-29 22:20:03 +0100

    Section 9 - mise en place de la stratégie JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 import { User } from './user.entity';
 
@@ -20,7 +21,7 @@ import { User } from './user.entity';
       }),
     }),
   ],
-  providers: [LocalStrategy, AuthService],
+  providers: [LocalStrategy, JwtStrategy, AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
