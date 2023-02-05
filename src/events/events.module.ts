de7e7031338c54e68dd4c0838c1fb8bf567245ac
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2023-02-05 10:15:58 +0100

    Section 11 - route get événements organisés par un utilisateur

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -6,10 +6,15 @@ import { Attendee } from './attendee.entity';
 import { EventsService } from './events.service';
 import { AttendeesService } from './attendees.service';
 import { EventAttendeesController } from './event-attendees.controller';
+import { EventsOrganizedByUserController } from './events-organized-by-user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Event, Attendee])],
-  controllers: [EventsController, EventAttendeesController],
+  controllers: [
+    EventsController,
+    EventAttendeesController,
+    EventsOrganizedByUserController,
+  ],
   providers: [EventsService, AttendeesService],
 })
 export class EventsModule {}
